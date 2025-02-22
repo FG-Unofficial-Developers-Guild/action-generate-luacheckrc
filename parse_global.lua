6efd5b085e3db93d81b8f124de4644607255e2b9
Wil T <wil.thieme@protonmail.com>
2025-02-21 22:18:56 -0500

    Update parse_global.lua

--- parse_global.lua
+++ parse_global.lua
@@ -199,7 +199,7 @@ local function writeDefinitionsToFile(defintitions, package, version)
 
 		for parent, fns in orderedPairs(defintitions[package]) do
 			local simpleParent = simpleName(parent)
-			if simpleParent ~= '' && not string.find(simpleParent, "%.") then
+			if simpleParent ~= '' and not string.find(simpleParent, "%.") then
 				local global = (simpleName(parent) .. ' = {\n\t\tread_only = false,\n\t\tfields = {\n\t' .. writeSubdefintions(fns) .. '\t},\n\t},')
 				table.insert(output, global)
 			elseif string.find(simpleParent, "%.") then
